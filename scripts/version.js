76649d588fdffa03512fe574900f6c41f463556e
wojtekxtx <wojtekx.tx@gmail.com>
2022-09-30 15:01:56 +0200

    remodelling version check

--- scripts/version.js
+++ scripts/version.js
@@ -5,11 +5,21 @@ const path = require('path');
 const pjPath = path.resolve(path.dirname(__filename), '../package.json');
 const versionPath = path.resolve(path.dirname(__filename), '../version');
 const distVersionPath = path.resolve(path.dirname(__filename), '../dist/sb-admin-angular/assets/version');
-
 const pj = require(pjPath);
 
-console.log(`### INFO: Current Version: ${pj.version}`);
-fs.writeFileSync(versionPath, pj.version);
-fs.writeFileSync(distVersionPath, pj.version);
+// fs.writeFileSync(versionPath, pj.version);
+// fs.writeFileSync(distVersionPath, pj.version);
 
+let determineVersion = function(){
+    if ((pj.version) && (pj.version >= 1.0)) {
+        console.log(`### INFO: Current Version: ${pj.version} is stable`);
+    } else {
+        console.log(pj.version);
+        break;
+    }
+
+    return pj.version;
+}
+
+// Export of modules
 module.exports = pj.version;
