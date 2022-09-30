60ce01b98c3f00b90c041400b0a8345865cc2d8c
wojtekxtx <wojtekx.tx@gmail.com>
2022-09-30 13:50:01 +0200

    Failure() logic redone

--- scripts/start.js
+++ scripts/start.js
@@ -4,8 +4,8 @@ const port = process.env.PORT || 4200;
 concurrently([
     { command: 'node scripts/pug-watch.js', name: 'PUG_WATCH', prefixColor: 'bgGreen.bold' },
     { 
-        command: `npm run ng -- serve --port ${port} --open`,
-        name: 'NG_SERVE', 
+        command: `npm run ng --serve --port ${port} --open`,
+        name: 'NG_SERVE',
         prefixColor: 'bgBlue.bold',
     }
 ], {
@@ -18,5 +18,10 @@ function success() {
 }
 
 function failure() {
-    console.log('Failure');
+    let detErr = document.getElementsByClassName('error_log').nodeValue;
+    if (detErr) {
+        console.log('Failure ' + detErr);
+    } else {
+        console.log('Failure with no logged message');
+    }
 }
